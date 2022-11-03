5d423b9dea21f21771f81e0e837a5e5c59d3a1ef
spencer2cai <spencer2cai@gmail.com>
2022-11-03 21:22:24 +0800

    asdfasdfa

--- async.cc
+++ async.cc
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2002-2010 Stephen Williams (steve@icarus.com)
- *
+ *sdfasdf
  *    This source code is free software; you can redistribute it
  *    and/or modify it in source code form under the terms of the GNU
  *    General Public License as published by the Free Software
